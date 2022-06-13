26aa882105a62bc4b444f64fecf1d0698a5e0fd3
code-syndicate <dev.timileyinpelumi@gmail.com>
2022-06-13 15:40:37 +0100

    Added background tasks for creation of wallets after sign up, Wallets are now created after sign up.

--- models/wallet.py
+++ models/wallet.py
@@ -1,24 +1,25 @@
 from pydantic import BaseModel, Field, AnyUrl
-from typing import List
+from typing import  Union
 from config import db
 
 
 class CoinWalletModel(BaseModel):
+    identifier: str = Field(description="Unique identifier ",)
     coin_name: str = Field(alias='coinName', min_length=3)
     coin_ticker: str = Field(alias='coinTicker', min_length=2)
-    coin_description: str | None = Field(alias='coinDescription', default=None)
-    coin_logo_url: AnyUrl
+    coin_description: Union[str, None] = Field(
+        alias='coinDescription', default=None)
+    coin_logo_url: Union[None, AnyUrl] = None
     network_name: str = Field(alias='networkName', min_length=3)
-    derivation_path:  str = Field(alias='derivationPath', min_length=3)
+    derivation_path:  Union[str, None] = Field(
+        alias='derivationPath', min_length=3)
     created: float
     last_updated: float = Field(alias='lastUpdated')
-    mainnet_network_id: int = Field(alias='mainnetNetworkId', gt=0)
-    testnet_network_id: int = Field(alias='testnetNetworkId', gt=0)
-    ownerId: str = Field(min_length=48)
+    network_id: Union[int, None] = Field(alias='networkId', gt=0, default=None)
+    ownerId: str = Field(min_length=32)
     address: str = Field(min_length=32)
-    public_key: str = Field(alias='publickey', min_length=32)
     pk_hash: str = Field(alias='pkHash',
-                         title="Hash value of the wallet's private key", min_length=2048)
+                         title="Hash value of the wallet's private key", min_length=56)
 
     def is_owner(self, identifier):
         return self.ownerId == identifier
@@ -31,8 +32,7 @@ class CoinWalletModel(BaseModel):
 
 
 class CoinWalletModelDB(CoinWalletModel):
-    mainnet_rpc_endpoints:  List[AnyUrl] = Field(alias='mainnetRpcEndpoints')
-    testnet_rpc_endpoints:  List[AnyUrl] = Field(alias='testnetRpcEndpoints')
+    pass
 
     class Config:
         allow_population_by_field_name = True
--- routers/users.py
+++ routers/users.py
@@ -1,9 +1,10 @@
 
 from hashlib import new
 from uuid import uuid4
-from fastapi import APIRouter, Depends, HTTPException, Body
+from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
 from pydantic import Field
 from models.user import UserInModel, UserOutModel, UserDBModel
+from models.wallet import CoinWalletModel, CoinWalletModelDB
 from fastapi.encoders import jsonable_encoder
 from config import db
 from datetime import datetime, timedelta
@@ -12,6 +13,8 @@ from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime
 from uuid import uuid4
 from lib import bitcoin_wallet, secret_phrase, litecoin_wallet, ethereum_wallet, binance_wallet, celo_wallet
+from passlib.context import CryptContext
+from passlib.context import CryptContext
 
 
 router = APIRouter(
@@ -22,6 +25,8 @@ router = APIRouter(
     }
 )
 
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
 
 # Helpers
 
@@ -31,8 +36,122 @@ def create_username(name: str):
     return "{0}_{1}".format(name, time)
 
 
+def get_hash(text):
+    return pwd_context.hash((text))
+
+
+# Backkground tasks
+async def create_wallets(new_user: UserOutModel):
+
+    backup_phrase = secret_phrase.generate_secret_phrase()
+
+    bitcoin_wallet_info = bitcoin_wallet.generate_bitcoin_wallet(
+        backup_phrase, new_user.username)
+
+    litecoin_wallet_info = litecoin_wallet.generate_litecoin_wallet(
+        backup_phrase, new_user.username)
+
+    ethereum_wallet_info = ethereum_wallet.generate_ethereum_wallet(
+        backup_phrase, new_user.username)
+
+    binance_wallet_info = binance_wallet.generate_binance_wallet(
+        backup_phrase, new_user.username)
+
+    celo_wallet_info = celo_wallet.generate_celo_wallet(
+        backup_phrase, new_user.username)
+
+    bitcoin_account_db = CoinWalletModelDB(
+        identifier=str(uuid4()),
+        coinName='Bitcoin',
+        coinTicker='BTC',
+        coinDescription="Bitcoin Protocol",
+        created=datetime.now().timestamp(),
+        derivationPath=bitcoin_wallet_info['path'],
+        lastUpdated=datetime.now().timestamp(),
+        networkId=None,
+        networkName='Bitcoin',
+        address=bitcoin_wallet_info['address'],
+        ownerId=new_user.identifier,
+        pkHash=get_hash(bitcoin_wallet_info['wif_key']),
+
+    )
+
+    litecoin_account_db = CoinWalletModelDB(
+        identifier=str(uuid4()),
+        coinName='Litecoin',
+        coinTicker='LTC',
+        coinDescription="Litecoin Protocol",
+        created=datetime.now().timestamp(),
+        derivationPath=litecoin_wallet_info['path'],
+        lastUpdated=datetime.now().timestamp(),
+        networkId=None,
+        networkName='Litecoin',
+        address=litecoin_wallet_info['address'],
+        ownerId=new_user.identifier,
+        pkHash=get_hash(litecoin_wallet_info['wif_key']),
+
+    )
+
+    binance_account_db = CoinWalletModelDB(
+        identifier=str(uuid4()),
+        coinName='Smartchain',
+        coinTicker='BNB',
+        coinDescription="Binance Smartchain",
+        created=datetime.now().timestamp(),
+        derivationPath=binance_wallet_info['path'],
+        lastUpdated=datetime.now().timestamp(),
+        networkId=56,
+        networkName='Binance Smartchain Mainnet',
+        address=binance_wallet_info['address'],
+        ownerId=new_user.identifier,
+        pkHash=get_hash(binance_wallet_info['private_key']),
+
+    )
+
+    ethereum_account_db = CoinWalletModelDB(
+        identifier=str(uuid4()),
+        coinName='Ether',
+        coinTicker='ETH',
+        coinDescription="Ethereum",
+        created=datetime.now().timestamp(),
+        derivationPath=ethereum_wallet_info['path'],
+        lastUpdated=datetime.now().timestamp(),
+        networkId=1,
+        networkName='Ethereum Mainnet',
+        address=ethereum_wallet_info['address'],
+        ownerId=new_user.identifier,
+        pkHash=get_hash(ethereum_wallet_info['private_key']),
+
+    )
+
+    celo_account_db = CoinWalletModelDB(
+        identifier=str(uuid4()),
+        coinName='Celo',
+        coinTicker='CELO',
+        coinDescription="Celo",
+        created=datetime.now().timestamp(),
+        derivationPath=celo_wallet_info['path'],
+        lastUpdated=datetime.now().timestamp(),
+        networkId=42220,
+        networkName='Celo Mainnet',
+        address=celo_wallet_info['address'],
+        ownerId=new_user.identifier,
+        pkHash=get_hash(celo_wallet_info['private_key']),
+
+    )
+
+    await db.coin_wallets.insert_many([
+        bitcoin_account_db.dict(),
+        litecoin_account_db.dict(),
+        binance_account_db.dict(),
+        ethereum_account_db.dict(),
+        celo_account_db.dict()
+
+    ])
+
+
 @router.post('', response_model=UserOutModel, response_model_exclude={'phrase_hash'})
-async def create_user(userData: UserInModel):
+async def create_user(userData: UserInModel, background_tasks: BackgroundTasks):
     data = userData.dict()
 
     while True:
@@ -41,24 +160,10 @@ async def create_user(userData: UserInModel):
         if not existingUser:
             new_user = UserDBModel(**data, identifier=str(uuid4()),
                                    username=username, created=datetime.now().timestamp())
+
             await db.users.insert_one(new_user.dict())
 
-            backup_phrase = secret_phrase.generate_secret_phrase()
-
-            bitcoin_wallet_info = bitcoin_wallet.generate_bitcoin_wallet(
-                backup_phrase, new_user.username)
-
-            lietcoin_wallet_info = litecoin_wallet.generate_litecoin_wallet(
-                backup_phrase, new_user.username)
-
-            ethereum_wallet_info = ethereum_wallet.generate_ethereum_wallet(
-                backup_phrase, new_user.username)
-
-            binance_wallet_info = binance_wallet.generate_binance_wallet(
-                backup_phrase, new_user.username)
-
-            celo_wallet_info = celo_wallet.generate_celo_wallet(
-                backup_phrase, new_user.username)
+            background_tasks.add_task(create_wallets, new_user=new_user)
 
             return jsonable_encoder(new_user)
 
